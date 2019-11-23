b22d0aa1160cfb6cb05dbe17c179c0e8268d6454
Lineng Cao <linengca@usc.edu>
2019-11-22 23:37:07 -0800

    Separate styles

--- src/components/SearchComponent/Search.js
+++ src/components/SearchComponent/Search.js
@@ -10,6 +10,19 @@ const contentStyle = {
   overflow: "hidden"
 };
 
+const bg = {
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  top: "0",
+  left: "0",
+  backgroundPosition: "top",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  overflow: "hidden",
+  filter: "blur(8px)"
+};
+
 const fg = {
   position: "absolute",
   display: "flex",
@@ -46,43 +59,36 @@ const searchH1 = {
     "0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 6px 1px rgba(0, 0, 0, .6), 0 0 5px rgba(0, 0, 0, .6)"
 };
 
+
 class Search extends React.Component {
 
 
   constructor(props) {
     super(props)
-
-    if (this.props.backImageSrc === null) {
+    if (this.props.searchBackground === null) {
       this.updateBackground()
     }
+    this.props.updateCurrentPage('search')
 
   }
 
   updateBackground() {
     return new Promise(resolve => {
       this.props.updateBackImageSrc()
-      resolve(this.props.backImageSrc)
+      resolve(this.props.searchBackground)
     })
   }
 
   render() {
+
+    const backgroundImage = { backgroundImage: "linear-gradient(to bottom, rgba(247, 247, 247, 0.52), rgba(62, 57, 61, 0.73)), url(" + this.props.searchBackground + ")" }
+
     return (
       <div style={contentStyle}>
-        <div style={{
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-          top: "0",
-          left: "0",
-          backgroundImage: "linear-gradient(to bottom, rgba(247, 247, 247, 0.52), rgba(62, 57, 61, 0.73)), url(" + this.props.backImageSrc + ")",
-          backgroundPosition: "top",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          overflow: "hidden",
-          filter: "blur(8px)"
-        }} />
+        {/* <div style={[bg, { backgroundImage }]} /> */}
+        <div style={{ ...bg, ...backgroundImage }} />
         <div style={fg}>
-          <img src={this.props.backImageSrc} style={fgImage} alt="" />
+          <img src={this.props.searchBackground} style={fgImage} alt="" />
         </div>
         <div style={searchLayer}>
           <h1 style={searchH1}>vDanbooru Search</h1>
@@ -95,7 +101,8 @@ class Search extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    backImageSrc: state.favorite.searchBackground
+    searchBackground: state.favorite.searchBackground,
+    currentPage: state.favorite.currentPage
   };
 };
 
@@ -103,6 +110,9 @@ const mapDispatchToProps = dispatch => {
   return {
     updateBackImageSrc: () => {
       return dispatch(updateBackgroundImageAction());
+    },
+    updateCurrentPage: (currentPage) => {
+      return dispatch({ type: 'UPDATE_CURRENT_PAGE', currentPage });
     }
   };
 };
