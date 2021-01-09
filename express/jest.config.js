cb03074d2e0e530e15296ef238838989e7989c93
Lineng Cao <linengca@usc.edu>
2021-01-09 02:13:21 -0800

    fix issues in heroku deployment of github action, add docker

--- express/jest.config.js
+++ express/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['./src'],
+  roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
--- express/src/controllers/safebooruController.ts
+++ express/src/controllers/safebooruController.ts
@@ -1,13 +1,12 @@
 import { Request, Response, Router } from 'express';
-import * as service from '../services/safeBooruService';
+import { getBooruImage } from '../services/safebooruService';
 
 const controller = (router: Router) => {
   const getImageAdvanced = (req: Request, res: Response): Promise<Response> => {
     const { type, t, n } = req.params;
-    return service
-      .getBooruImage(type, t, Number(n))
-      .then((data) => res.json(data))
-      .catch((error) =>
+    return getBooruImage(type, t, Number(n))
+      .then((data: string | object) => res.json(data))
+      .catch((error: Error) =>
         res.status(500).json({
           error: error.message,
         })
